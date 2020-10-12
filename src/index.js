7c62ae2320df5ee5fd8afb1063cd3b71a3641cc9
Ahasanul Hoque <ahoque24@gmail.com>
2020-10-12 19:53:43 -0400

    Added logic for creating new todo object from form submitted by user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {toDo} from './todo-logic.js';
 import {renderPage} from './render-page.js'
-import {createToDo} from './render-todo.js'
+import {showToDoForm, removeToDoForm} from './render-todo.js'
 
 //Test toDo object
 const hw = toDo('Homework', 'Homework', '12/1/20', 1);
@@ -13,6 +13,30 @@ content.onclick = function(e){
     let button = e.target;
     if (button.className == 'new-to-do'){
         //Call a form function from a render module
-        createToDo(content);
+        showToDoForm(content);
+        let form = document.querySelector('form');
+        form.onclick = function(f){
+            let newButton = f.target;
+            if (newButton.tagName == 'BUTTON'){
+                removeToDoForm(content,form);
+                if (newButton.id = 'submit-to-do'){
+                    //If todo form is submitted instead of canceled
+                    //create new todo object
+                    let newToDo = toDo(form.elements.namedItem('title').value,
+                                        form.elements.namedItem('description').value,
+                                        form.elements.namedItem('date').value,
+                                        form.elements.namedItem('priority').value);
+                    console.log(newToDo);
+                }
+            }
+        }
     }
 }
+
+//Each project will be an array
+//When a new ToDo is created, it will be added to project array
+//Iterate over project array and use ToDo.priority to determine where
+//to push ToDo into project array
+
+//Use data-index for arrays and submit buttons?
+//Or only have submit button for shown project/array
--- src/render-todo.js
+++ src/render-todo.js
@@ -2,8 +2,8 @@
 //Show form on page
 //On click of submit button, return user values and close form
 
-
-const createToDo = (content) => {
+//Add the rest of the required ToDo properties to this form
+const showToDoForm = (content) => {
     const newForm = document.createElement('form');
 
     const toDoTitle = document.createElement('h3');
@@ -21,12 +21,53 @@ const createToDo = (content) => {
     titleInput.setAttribute('placeholder', 'ToDo Title');
     newForm.appendChild(titleInput);
     
+    const descriptionLabel = document.createElement('label');
+    descriptionLabel.setAttribute('for', 'description');
+    descriptionLabel.innerHTML = 'ToDo Description';
+    newForm.appendChild(descriptionLabel);
+
+    const descriptionInput = document.createElement('input');
+    descriptionInput.setAttribute('name', 'description');
+    descriptionInput.setAttribute('type', 'text');
+    descriptionInput.setAttribute('placeholder', 'ToDo Description');
+    newForm.appendChild(descriptionInput);
+
+    const dateLabel = document.createElement('label');
+    dateLabel.setAttribute('for', 'date');
+    dateLabel.innerHTML = 'Due Date';
+    newForm.appendChild(dateLabel);
+
+    const dateInput = document.createElement('input');
+    dateInput.setAttribute('name', 'date');
+    dateInput.setAttribute('type', 'text');
+    dateInput.setAttribute('placeholder', 'Due Date');
+    newForm.appendChild(dateInput);
+
+    const priorityLabel = document.createElement('label');
+    priorityLabel.setAttribute('for', 'priority');
+    priorityLabel.innerHTML = 'ToDo priority';
+    newForm.appendChild(priorityLabel);
+
+    const priorityInput = document.createElement('input');
+    priorityInput.setAttribute('name', 'priority');
+    priorityInput.setAttribute('type', 'number');
+    priorityInput.setAttribute('min', '1');
+    priorityInput.setAttribute('placeholder', 'ToDo Priority');
+    newForm.appendChild(priorityInput);
+
     const submit = document.createElement('button');
     submit.setAttribute('type', 'button');
+    submit.id = 'submit-to-do';
     submit.innerHTML = 'Add ToDo';
     newForm.appendChild(submit);
 
     content.appendChild(newForm);
+
+    //Return user values so index can create new ToDo object?
 }
 
-export {createToDo};
+const removeToDoForm = (content, form) => {
+    content.removeChild(form);
+}
+
+export {showToDoForm, removeToDoForm};
